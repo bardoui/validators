db31e2a767ac20335fbab8654764cac5c9920c08
M Ekramy <m.ajamekramy@gmail.com>
2021-09-12 20:30:45 +0430

    fix uuid validator

--- src/validators/uuid.ts
+++ src/validators/uuid.ts
@@ -15,7 +15,7 @@ export function registerUUIDValidator(defaultMessage = "uuid") {
             exclusive: true,
             test: (v: Maybe<string>) =>
                 !v ||
-                /^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}+$/.test(
+                /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
                     s(v)
                 ),
         })
